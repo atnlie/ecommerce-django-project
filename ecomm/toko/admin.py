3a62d8a5611af648f777c08b278e55bb4eb0d902
Anton <heni.kurniyanto@kuncie.com>
2023-04-12 08:38:04 +0700

    feat(admin): add to admin site

--- ecomm/toko/admin.py
+++ ecomm/toko/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import ProdukItem
+from .models import ProdukItem, OrderProdukItem, Order
 
 class ProdukItemAdmin(admin.ModelAdmin):
     list_display = ['nama_produk','harga', 'harga_diskon', 'slug',
                     'deskripsi', 'gambar', 'label', 'kategori']
 
+class OrderProdukItemAdmin(admin.ModelAdmin):
+    list_display = ['user', 'ordered', 'produkItem', 'quantity']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'tanggal_mulai', 'tanggal_order', 'ordered']
+
 admin.site.register(ProdukItem, ProdukItemAdmin)
+admin.site.register(OrderProdukItem, OrderProdukItemAdmin)
+admin.site.register(Order, OrderAdmin)
